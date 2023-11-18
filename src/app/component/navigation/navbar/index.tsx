82bd57adf9f7844f9ff4d008f99da10590f477f0
Erwin Susanto <erwins71297@gmail.com>
2023-11-18 12:30:34 +0700

    Change navbar z-position

--- src/app/component/navigation/navbar/index.tsx
+++ src/app/component/navigation/navbar/index.tsx
@@ -9,7 +9,7 @@ const Navbar = ({
 }) => {
   return (
     <>
-      <div className="w-full h-20 bg-yellow-400 sticky top-0">
+      <div className="w-full h-20 bg-yellow-400 sticky top-0 z-10">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <a href="/" className={styles.homebutton}>
