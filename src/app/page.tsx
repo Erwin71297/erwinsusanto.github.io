ed7e8f4cdbbb0893ba6b556b9a1acc768b1f3f84
Erwin Susanto <erwins71297@gmail.com>
2023-11-27 17:39:03 +0700

    CV Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
             </Link>
           </p>
           <p className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <Link className="mb-3 text-2xl font-semibold" href="assets/CVErwin.pdf" download>
+            <Link className="mb-3 text-2xl font-semibold" href="assets/CV.pdf" download>
               Download CV
             </Link>
           </p>
